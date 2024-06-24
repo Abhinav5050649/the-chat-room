266fa74074bd48fdf2c7bdd8b8e64b1cca4b1e1f
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-06-24 16:05:05 +0530

    Added messages component

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,9 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+const Message = ({ message }) => {
+    const [user] = useAuthState(auth);
 
-function Message({ message }) {
     return (
-        <div className="Message">
-            <h3>{message}</h3>
+        <div
+            className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
+            <img
+                className="chat-bubble__left"
+                src={message.avatar}
+                alt="user avatar"
+            />
+            <div className="chat-bubble__right">
+                <p className="user-name">{message.name}</p>
+                <p className="user-message">{message.text}</p>
+            </div>
         </div>
     );
-}
+};
+export default Message;
